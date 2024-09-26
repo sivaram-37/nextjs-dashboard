438932e0f1f48e48e19490f8d695277d9ec5a811
sivaram <sivaram3710@gmail.com>
2024-09-26 17:21:05 +0530

    fixed some error

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import AcmeLogo from "@/app/ui/acme-logo";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import Image from "next/image";
-import styles from "@/app/ui/home.module.css";
 import { lusitana } from "./ui/fonts";
 
 export default function Page() {
@@ -30,7 +29,6 @@ export default function Page() {
 					</Link>
 				</div>
 				<div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-					{/* Add Hero Images Here */}
 					<Image
 						src="/hero-desktop.png"
 						width={1000}
